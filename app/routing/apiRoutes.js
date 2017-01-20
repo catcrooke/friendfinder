2d2435088e5deca17178dfbdb1024b68cbed615b
Catherine Crooke <Cat@Catherines-MacBook-Pro.local>
2017-01-19 18:20:14 -0600

    updated code in js and html pages

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,28 +2,26 @@
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. 
 // This route will also be used to handle the compatibility logic.
+var friends = [];
 
-// Route to api/friends
-app.get("/api/friends", function(req, res) {
-    res.json(friends);
-});
+module.exports = function apiRoutes(app) {
+    // Route to api/friends
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
 
+    // Route to create friends JSON
+    app.post("/api/friends", function(req, res) {
+        var newFriend = req.body;
+        var listType;
+        console.log(newFriend);
+        // if ("") {
+        //     listType = 'friends list';
+        //     friends.push(newFriend);
+        // }
 
-// Route to create friends JSON
-app.post("/api/friends", function(req, res) {
-    var newFriends = req.body;
-    var listType;
-    console.log(newFriends);
-    // if (friends.length < 5) {
-    //     listType = 'friendsList';
-    //     friends.push(newFriends);
-    // } else {
-    //     listType = 'waitlist';
-    //     waitlist.push(newTable);
-    // }
+        console.log('friends: ' + JSON.stringify(friends));
+        res.send(listType);
 
-
-    console.log('friends: ' + JSON.stringify(friends));
-    res.send(listType);
-
-});
+    });
+};

d3809b2136882047aefd221f4948106cd786f081
Catherine Crooke <Cat@Catherines-MacBook-Pro.local>
2017-01-19 19:23:33 -0600

    fixed routes in js files and required path

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,7 @@
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. 
 // This route will also be used to handle the compatibility logic.
+var path = require("path");
 var friends = [];
 
 module.exports = function apiRoutes(app) {
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,15 @@
-module.exports = function htmlRoutes(app) {
-    // Your htmlRoutes.js file should include two routes:
-    // A GET Route to /survey which should display the survey page.
-    app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "survey.html"));
-    });
-    // A default USE route that leads to home.html which displays the home page.
-    // Basic route that sends the user first to the AJAX Page
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "home.html"));
-    });
+    var path = require("path");
 
-};
+    module.exports = function htmlRoutes(app) {
+        // Your htmlRoutes.js file should include two routes:
+        // A GET Route to /survey which should display the survey page.
+        app.get("/survey", function(req, res) {
+            res.sendFile(path.join(__dirname, "../public/survey.html"));
+        });
+        // A default USE route that leads to home.html which displays the home page.
+        // Basic route that sends the user first to the AJAX Page
+        app.get("/", function(req, res) {
+            res.sendFile(path.join(__dirname, "../public/home.html"));
+        });
+
+    };
